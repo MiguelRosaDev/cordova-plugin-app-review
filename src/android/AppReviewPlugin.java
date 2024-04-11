a3cb1ad0fba1323a89ab8fba030b519e4c94f721
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-04-11 19:26:55 +0100

    Update AppReviewPlugin.java

--- src/android/AppReviewPlugin.java
+++ src/android/AppReviewPlugin.java
@@ -18,7 +18,7 @@ import by.chemerisuk.cordova.support.CordovaMethod;
 import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
 
 public class AppReviewPlugin extends ReflectiveCordovaPlugin {
-    @CordovaMethod(ExecutionThread.WORKER)
+    @CordovaMethod()
     private void requestReview(CallbackContext callbackContext) throws Exception {
         Activity activity = cordova.getActivity();
         ReviewManager manager = ReviewManagerFactory.create(activity);
